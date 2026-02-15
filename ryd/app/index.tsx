cd605db1076487b514c3e5c31d2d212574ee0e95
Matheshwara <matheshwarar@gmail.com>
2026-02-16 01:43:38 +0530

    UPDATE: file structure, assets, and fonts

---
+++ ryd/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Home =() => {
+    return(
+        <SafeAreaView>
+            <Text>
+                Home
+            </Text>
+        </SafeAreaView>
+    );
+
+};
+
+export default Home;
